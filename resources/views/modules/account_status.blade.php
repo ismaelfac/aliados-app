35b1f927d074999b587f82907016629fdbc614c7
ISMAEL E. LASTRE ALVAREZ <ismaelfac@gmail.com>
2021-06-01 09:04:08 -0500

    account_status debuging

--- resources/views/modules/account_status.blade.php
+++ resources/views/modules/account_status.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-12 ml-auto mr-auto">
   <div class="card card-upgrade">
     <div class="card-header text-center">
-      <h4 class="card-title">Factura No 254620</h3>
+      <h4 class="card-title"></h3>
     </div>
     <div class="card-body">
       <div class="row">
@@ -21,10 +21,9 @@
                                 <th scope="col">#</th>
                                 <th scope="col">Cuenta</th>
                                 <th scope="col">Cliente</th>
-                                <th scope="col">Suma Saldos</th>
-                                <th scope="col">Cumplimiento</th>
-                                <th scope="col">Vence</th>
-                                <th scope="col">Valor Total</th>
+                                <th scope="col">Saldos</th>
+                                <th scope="col">Cump %</th>
+                                <th scope="col">Acuerdo Pago</th>
                                 <th scope="col">Opciones</th>
                             </tr>
                         </thead>
@@ -38,16 +37,11 @@
                                         </label>
                                     </div>
                                 </td>
-                                <td>
-                                <a type="button" href="{{ url('/to_pay') }}">
-                                    <img width="70px" height="50px" src="https://colegiofervan.edu.co/wp-content/uploads/2020/02/BotonPSE.png" alt="" srcset="" />
-                                </a>
-                                </td>
-                                <td>254620</td>
-                                <td>72288959</td>
-                                <td>2021/05</td>
-                                <td>2021-05-20</td>
-                                <td>$ 1.235.000.00</td>
+                                <td>1</td>
+                                <td>TERNERA DE CAMPO</td>
+                                <td>$4.182.000</td>
+                                <td>62,5%</td>
+                                <td><a href="#" class="btn btn-outline-success">Vigente</a></td>
                                 <td class="td-actions text-right">
                                     <a type="button" href="{{ url('details_payments',1) }}" rel="tooltip" title="" class="btn btn-info btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Edit Task">
                                         <i class="now-ui-icons ui-2_settings-90"></i>
@@ -66,76 +60,15 @@
                                         </label>
                                     </div>
                                 </td>
-                                <td>
-                                    <a type="button" href="{{ url('to_pay',1) }}" rel="tooltip" title="" class="btn btn-danger btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Remove">
-                                        <i class="now-ui-icons ui-1_simple-remove"></i>
+                                <td>2</td>
+                                <td>IGT</td>
+                                <td>$4.629.405</td>
+                                <td>80%</td>
+                                <td><a href="#" class="btn btn-primary">Finalizado <span>-1</span></a></td>
+                                <td class="td-actions text-right">
+                                    <a type="button" href="{{ url('details_payments',1) }}" rel="tooltip" title="" class="btn btn-info btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Edit Task">
+                                        <i class="now-ui-icons ui-2_settings-90"></i>
                                     </a>
-                                </td>
-                                <td>254620</td>
-                                <td>72288959</td>
-                                <td>2021/05</td>
-                                <td>2021-05-20</td>
-                                <td>$ 1.235.000.00</td>
-                                <td class="td-actions text-right">
-                                    <button type="button" rel="tooltip" title="" class="btn btn-info btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Edit Task">
-                                        <i class="now-ui-icons ui-2_settings-90"></i>
-                                    </button>
-                                    <button type="button" rel="tooltip" title="" class="btn btn-danger btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Remove">
-                                        <i class="now-ui-icons ui-1_simple-remove"></i>
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="form-check">
-                                        <label class="form-check-label">
-                                            <input class="form-check-input" type="checkbox">
-                                            <span class="form-check-sign"></span>
-                                        </label>
-                                    </div>
-                                </td>
-                                <td>
-                                    <button type="button" rel="tooltip" title="" class="btn btn-success btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Add">
-                                        <i class="now-ui-icons ui-1_check"></i>
-                                    </button>
-                                </td>
-                                <td>254620</td>
-                                <td>72288959</td>
-                                <td>2021/05</td>
-                                <td>2021-05-20</td>
-                                <td>$ 1.235.000.00</td>
-                                <td class="td-actions text-right">
-                                    <button type="button" rel="tooltip" title="" class="btn btn-info btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Edit Task">
-                                        <i class="now-ui-icons ui-2_settings-90"></i>
-                                    </button>
-                                    <button type="button" rel="tooltip" title="" class="btn btn-danger btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Remove">
-                                        <i class="now-ui-icons ui-1_simple-remove"></i>
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="form-check">
-                                        <label class="form-check-label">
-                                            <input class="form-check-input" type="checkbox">
-                                            <span class="form-check-sign"></span>
-                                        </label>
-                                    </div>
-                                </td>
-                                <td>
-                                    <button type="button" rel="tooltip" title="" class="btn btn-success btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Add">
-                                        <i class="now-ui-icons ui-1_check"></i>
-                                    </button>
-                                </td>
-                                <td>254620</td>
-                                <td>72288959</td>
-                                <td>2021/05</td>
-                                <td>2021-05-20</td>
-                                <td>$ 1.235.000.00</td>
-                                <td class="td-actions text-right">
-                                    <button type="button" rel="tooltip" title="" class="btn btn-info btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Edit Task">
-                                        <i class="now-ui-icons ui-2_settings-90"></i>
-                                    </button>
                                     <button type="button" rel="tooltip" title="" class="btn btn-danger btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Remove">
                                         <i class="now-ui-icons ui-1_simple-remove"></i>
                                     </button>
