ff781959491120ce391418d62bef753b12606ba4
Ismael E. Lastre Alvarez <ismaelfac@gmail.com>
2021-06-03 12:06:15 -0500

    spatie debuging

---
+++ database/migrations/2021_06_03_165718_create_customers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id();
+            $table->enum('state_customer', ['Activo', 'Inactivo', 'Suspendido'])->default('Inactivo');
+            $table->enum('type_dni', ['CC', 'NIT'])->default('NIT');
+            $table->string('dni', 30);
+            $table->string('business_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('name')->nullable();
+            $table->string('slug')->unique();
+            $table->string('phone')->nullable();
+            $table->string('email', 60)->unique();
+            $table->boolean('state')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +13,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->truncateTables([
+            'password_resets',
+            'users',
+            'role_has_permissions',
+            'permissions',
+            'roles',
+            'customers'
+        ]);
+        $this->call([
+            PermissionsTableSeeder::class,
+            //CustomersTableSeeder::class,
+            UserTableSeeder::class
+        ]);
+    }
+    protected function truncateTables(array $tables)
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;'); // Desactivamos la revisión de claves foráneas
+        foreach ($tables as $table) {
+            DB::table($table)->truncate();
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;'); // Reactivamos la revisión de claves foráneas
     }
 }
